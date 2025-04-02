b3cff3e33b9790381f6497e8f64a675691cf5f33
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-04-02 17:59:15 +0300

    Vanha projekti meni totaaliseen bugiin, joten kopioin sisällön, poistin sen ja perustin uuden projektin tänne

---
+++ src/main/java/k25/kaatokerho/KaatokerhoApplication.java
@@ -0,0 +1,13 @@
+package k25.kaatokerho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaatokerhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaatokerhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/k25/kaatokerho/KaatokerhoApplicationTests.java
@@ -0,0 +1,13 @@
+package k25.kaatokerho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaatokerhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
