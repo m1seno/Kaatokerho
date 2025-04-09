ee4d22dc558e270b0381d64ecf7a0bbc88c0384b
Miika Nordblad <miika.nordblad@myy.haaga-helia.fi>
2025-04-09 13:30:00 +0300

    WebSecurityConfig -luokkaa muokattu niin, että kotisivulle pääsee ilman admin tunnuksia

--- src/main/java/k25/kaatokerho/WebSecurityConfig.java
+++ src/main/java/k25/kaatokerho/WebSecurityConfig.java
@@ -1,16 +1,16 @@
 package k25.kaatokerho;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled= true)
@@ -23,25 +23,31 @@ public class WebSecurityConfig {
     }
 
     private static final AntPathRequestMatcher[] WHITE_LIST_URLS = {
-        new AntPathRequestMatcher("/home"),
-        new AntPathRequestMatcher("/login"),
-        new AntPathRequestMatcher("/css/**"),
-        new AntPathRequestMatcher("/js/**"),
-        new AntPathRequestMatcher("/images/**"),
+        new AntPathRequestMatcher("/home**"),
+        new AntPathRequestMatcher("/login**")
     };
 
     @Bean
-public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers(WHITE_LIST_URLS).permitAll() // näihin ei vaadita kirjautumista
-            .anyRequest().authenticated() // muihin vaaditaan
-        )
-        .formLogin(withDefaults())
-        .logout(logout -> logout.permitAll());
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-    return http.build();
-}
+        http.authorizeHttpRequests(
+            authorize -> authorize
+            .requestMatchers(antMatcher("/css/**")).permitAll()
+            .requestMatchers(antMatcher("/images/**")).permitAll()
+            .requestMatchers(WHITE_LIST_URLS).permitAll()
+            .anyRequest().authenticated())
+            .headers(headers ->
+            headers.frameOptions(frameOptions -> frameOptions
+            .disable()))
+            .formLogin(form -> form
+                .defaultSuccessUrl("/home", true)
+                .permitAll())
+            .logout(logout -> logout
+            .logoutSuccessUrl("/home?logout")
+            .permitAll())
+            .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
