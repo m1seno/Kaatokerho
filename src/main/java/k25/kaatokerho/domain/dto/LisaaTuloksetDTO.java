56f0109eb7b689cf80e261ec4b10bcb7742a9b6b
Miika Nordblad <bhm247@myy.haaga-helia.fi>
2025-11-05 17:16:07 +0200

    Konstruktorit tehty LisaaTulokseDTO

--- src/main/java/k25/kaatokerho/domain/dto/LisaaTuloksetDTO.java
+++ src/main/java/k25/kaatokerho/domain/dto/LisaaTuloksetDTO.java
@@ -5,9 +5,15 @@ import java.util.List;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LisaaTuloksetDTO {
 
     @NotNull(message = "GP id on pakollinen")
