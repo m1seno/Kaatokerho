37bb868d9ff63fb0719d2c345c2803b25908ddcd
Miika Nordblad <bhm247@myy.haaga-helia.fi>
2025-11-21 12:07:51 +0200

    Sisäänkirjautuessa ResponseLoginDTO palauttaa myös Keilaaja-olion

--- src/main/java/k25/kaatokerho/domain/dto/ResponseLoginDTO.java
+++ src/main/java/k25/kaatokerho/domain/dto/ResponseLoginDTO.java
@@ -1,13 +1,22 @@
 package k25.kaatokerho.domain.dto;
 
-public class ResponseLoginDTO {
-    private String token;
+import k25.kaatokerho.domain.Keilaaja;
 
-    public ResponseLoginDTO(String token) {
+public class ResponseLoginDTO {
+
+    private final String token;
+    private final ResponseKeilaajaDTO keilaaja;
+
+    public ResponseLoginDTO(String token, ResponseKeilaajaDTO keilaaja) {
         this.token = token;
+        this.keilaaja = keilaaja;
     }
 
     public String getToken() {
         return token;
+    }
+
+    public ResponseKeilaajaDTO getKeilaaja() {
+        return keilaaja;
     }
 }
--- src/main/java/k25/kaatokerho/web/AuthController.java
+++ src/main/java/k25/kaatokerho/web/AuthController.java
@@ -1,5 +1,7 @@
 package k25.kaatokerho.web;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,9 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.validation.Valid;
+import k25.kaatokerho.domain.Keilaaja;
 import k25.kaatokerho.domain.dto.LoginDTO;
+import k25.kaatokerho.domain.dto.ResponseKeilaajaDTO;
 import k25.kaatokerho.domain.dto.ResponseLoginDTO;
 import k25.kaatokerho.service.JwtService;
+import k25.kaatokerho.domain.KeilaajaRepository;
 
 @RestController
 @RequestMapping("/api/login")
@@ -27,12 +32,15 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final KeilaajaRepository keilaajaRepo;
 
     public AuthController(AuthenticationManager authenticationManager, JwtService jwtService,
-            UserDetailsService userDetailsService) {
+            UserDetailsService userDetailsService,
+            KeilaajaRepository keilaajaRepo) {
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
+        this.keilaajaRepo = keilaajaRepo;
     }
 
     @PostMapping
@@ -44,8 +52,20 @@ public class AuthController {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Bad credentials");
         }
 
+        Optional<Keilaaja> keilaaja = keilaajaRepo.findByKayttajanimi(loginDTO.getKayttajanimi());
+
+        ResponseKeilaajaDTO dto = new ResponseKeilaajaDTO(
+            keilaaja.get().getKeilaajaId(),
+            keilaaja.get().getEtunimi(),
+            keilaaja.get().getSukunimi(),
+            keilaaja.get().getSyntymapaiva(),
+            keilaaja.get().getAktiivijasen(),
+            keilaaja.get().getAdmin(),
+            keilaaja.get().getKayttajanimi()
+        );
+
         UserDetails userDetails = userDetailsService.loadUserByUsername(loginDTO.getKayttajanimi());
         String jwt = jwtService.generateToken(userDetails);
-        return ResponseEntity.ok(new ResponseLoginDTO(jwt));
+        return ResponseEntity.ok(new ResponseLoginDTO(jwt, dto));
     }
 }
